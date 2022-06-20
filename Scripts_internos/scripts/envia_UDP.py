009417c5dc6fb42a37b125f4fa75133effb7d0e5
root <pgf53@hotmail.com>
2022-06-20 19:01:56 +0200

    mejorado proceso de re-envio

--- Scripts_internos/scripts/envia_UDP.py
+++ Scripts_internos/scripts/envia_UDP.py
@@ -26,15 +26,6 @@ MESSAGE = MESSAGE.encode()
 
 while True:
 	sock.sendto(MESSAGE, (UDP_IP_SEND, UDP_PORT_SEND))	#Enviamos mensaje UDP
-	with open('send.log', "a+") as file_object:
-		# Move read cursor to the start of file.
-		file_object.seek(0)
-		# If file is not empty then append '\n'
-		recibido = file_object.read(100)
-		if len(recibido) > 0:
-			file_object.write("\n")
-		 # Append text at the end of file
-		file_object.write("Mensaje: " + UDP_IP_LISTEN + separador + str(UDP_PORT_LISTEN) + separador + TAREA + separador + FIN + " to: " + str(UDP_IP_SEND) + ":" + str(UDP_PORT_SEND))
 
 	#Esperamos 5 segundos ACK
 	#Si no lo recibimos, reenviamos.
@@ -43,6 +34,38 @@ while True:
 	ready = select.select([sock], [], [], TIEMPO_ENTRE_REINTENTOS)
 	if ready[0]:	
 		data, addr = sock.recvfrom(1024)
+
+		with open('send.log', "a+") as file_object:
+			# Move read cursor to the start of file.
+			file_object.seek(0)
+			# If file is not empty then append '\n'
+			recibido = file_object.read(100)
+			if len(recibido) > 0:
+				file_object.write("\n")
+			 # Append text at the end of file
+			file_object.write("Mensaje recibido del servidor: " + str(data.decode()))
+
+	else:
+		with open('send.log', "a+") as file_object:
+			# Move read cursor to the start of file.
+			file_object.seek(0)
+			# If file is not empty then append '\n'
+			recibido = file_object.read(100)
+			if len(recibido) > 0:
+				file_object.write("\n")
+			 # Append text at the end of file
+			file_object.write("Sin datos que leer. NO RESPUESTA DEL SERVIDOR")
+
+
+	with open('send.log', "a+") as file_object:
+		# Move read cursor to the start of file.
+		file_object.seek(0)
+		# If file is not empty then append '\n'
+		recibido = file_object.read(100)
+		if len(recibido) > 0:
+			file_object.write("\n")
+		 # Append text at the end of file
+		file_object.write("Mensaje enviado por cliente: " + UDP_IP_LISTEN + separador + str(UDP_PORT_LISTEN) + separador + TAREA + separador + FIN + " to: " + str(UDP_IP_SEND) + ":" + str(UDP_PORT_SEND))
 
 	if data == b"ACK":
 		print("ACK recibido")
@@ -69,7 +92,6 @@ while True:
 			 # Append text at the end of file
 			file_object.write("Equipo: " + UDP_IP_LISTEN + " Instancia: " + TAREA + " ACK NO recibido")
 
-		sock.sendto(MESSAGE, (UDP_IP_SEND, UDP_PORT_SEND))
 
 		NUM_INTENTOS = NUM_INTENTOS + 1
 		if NUM_INTENTOS == NUM_MAX_REINTENTOS:
