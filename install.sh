0e24c12c42782be273bcaa0dc770e53b27ded4c7
root <pgf53@hotmail.com>
2022-07-04 17:17:12 +0200

    cloud limpio

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-EQUIPOS="02 03 05-15 17-28"
-. Scripts_internos/scripts/cloud_config_interna.conf "${EQUIPOS}"
-
-for i in ${EQUIPOS_LT}; do
-	${SCP_COMANDO_KEY} "install_nemesida.sh"	"${USER_REMOTO}"@${PREFIJO_NOMBRE_EQUIPO}$i://opt
-	${SSH_COMANDO_KEY} "${USER_REMOTO}"@${PREFIJO_NOMBRE_EQUIPO}$i "cd /opt; ./install_nemesida.sh" 
-done
-
-
+#Dependencias para usar cloud
+yum install -y inotify-tools sshpass python36 byobu dialog git
 
--- test/README.txt
+++
@@ -1,3 +0,0 @@
-#########PRUEBAS#############
-
-Pruebas realizadas con objeto de comprobar el correcto funcionamiento de la herramienta Cloud.
